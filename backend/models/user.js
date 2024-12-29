78ff6a2e4f28d3ce689b36e109ff94a38c9d5f59
miriam <maviles221@gmail.com>
2024-12-29 12:34:08 -0600

    update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,9 @@ const SALT_ROUNDS = 6;
 
 const userSchema = new Schema(
   {
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: {
       type: String,
       unique: true,
