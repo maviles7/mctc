598f5746b57ecc6f6b5dc55373b11dd7800805f5
miriam <maviles221@gmail.com>
2024-12-31 16:08:08 -0500

    backend show functionality

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -3,6 +3,7 @@ const Post = require('../models/posts');
 module.exports = {
     index,
     create,
+    show,
 };
 
 // INDEX FUNCTIONALITY
@@ -16,6 +17,18 @@ async function index(req, res) {
         res.status(500).json({err});
     }
 };
+
+// SHOW FUNCTIONALITY
+async function show(req, res) {
+    try {
+        const post = await Post.findById(req.params.id)
+            .populate('owner')
+            //.populate({ path: "comments", populate: { path: "owner" } });
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({err});
+    }
+}
 
 // CREATE FUNCTIONALITY
 async function create(req, res) {
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -8,6 +8,9 @@ const postsCtrl = require('../controllers/posts');
 // GET /api/posts --> INDEX FUNCTIONALITY 
 router.get('/', postsCtrl.index);
 
+// GET /api/posts/:id --> SHOW FUNCTIONALITY
+router.get('/:id', postsCtrl.show);
+
 // POST /api/posts --> CREATE FUNCTIONALITY
 router.post('/', postsCtrl.create);
 
