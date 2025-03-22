f3dabd74923338741a540c689f60c4e91c8442fa
miriam <maviles221@gmail.com>
2025-03-22 12:24:44 -0500

    deleted amigo FE on amigolistpage

--- frontend/src/pages/AmigoPostListPage/AmigoPostListPage.jsx
+++ frontend/src/pages/AmigoPostListPage/AmigoPostListPage.jsx
@@ -7,6 +7,7 @@ export default function AmigoPostListPage() {
   const { userId } = useParams();
   const [posts, setPosts] = useState([]);
   const [isAmigoAdded, setIsAmigoAdded] = useState(false); // State to track if amigo is added
+  const [isAmigoDeleted, setIsAmigoDeleted] = useState(false); // State to track if amigo is deleted
 
 
   useEffect(() => {
@@ -27,6 +28,15 @@ export default function AmigoPostListPage() {
     }
   };
 
+  const handleDeleteAmigo = async () => {
+    try {
+      await amigoService.deleteAmigo(userId); // Call the service function to delete amigo
+      setIsAmigoDeleted(true); // Update the state to indicate amigo is deleted
+    } catch (error) {
+      console.error('Error deleting amigo:', error);
+    }
+  };
+
   const username = posts[0]?.owner?.username || '';
 
   return (
@@ -34,6 +44,9 @@ export default function AmigoPostListPage() {
       <h1>{username ? `${username}` : 'loading...' }</h1>
       <button onClick={handleAddAmigo} disabled={isAmigoAdded}>
         {isAmigoAdded ? 'Amigo Added' : 'Add Amigo'}
+      </button>
+      <button onClick={handleDeleteAmigo} disabled={isAmigoDeleted}>
+        {isAmigoDeleted ? 'Amigo Deleted' : 'Delete Amigo'}
       </button>
       {posts.length > 0 ? (
         <ul>
