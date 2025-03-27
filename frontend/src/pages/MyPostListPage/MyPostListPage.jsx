33b3d672046fed7a650b8a69cc85fa0dfc1bfeba
miriam <maviles221@gmail.com>
2025-03-27 17:38:10 -0500

    mypostlistpage CSS done

--- frontend/src/pages/MyPostListPage/MyPostListPage.jsx
+++ frontend/src/pages/MyPostListPage/MyPostListPage.jsx
@@ -5,7 +5,7 @@ import * as amigoService from "../../services/amigoService";
 import * as closetService from "../../services/closetService";
 
 export default function MyPostListPage({ posts, user }) {
-  console.log('user:', user);
+  console.log("user:", user);
 
   const navigate = useNavigate();
 
@@ -27,13 +27,12 @@ export default function MyPostListPage({ posts, user }) {
     fetchClosetItems();
   }, []);
 
-  console.log('amigos:', amigosInfo);
+  console.log("amigos:", amigosInfo);
 
   const handleDeleteAmigo = async (amigoId) => {
     const updatedAmigos = await amigoService.deleteAmigo(amigoId);
-    setAmigosInfo({ amigos: updatedAmigos});
-    navigate('/posts');
-
+    setAmigosInfo({ amigos: updatedAmigos });
+    navigate("/posts");
   };
 
   const handleRemoveFromCloset = async (postId) => {
@@ -47,46 +46,62 @@ export default function MyPostListPage({ posts, user }) {
 
   return (
     <>
-      <div>
-        <h1>my posts</h1>
-        <ul>
-        {posts
-          .filter((post) => post.owner._id === user._id)
-          .map((post) => (
-            <Link key={post._id} to={`/posts/${post._id}`}>
-                <li>{post.title}</li>
-            </Link>
-          ))}
-          </ul>
-      </div>
-      <div>
-        <h1>my amigos</h1>
-        {amigosInfo.amigos && amigosInfo.amigos.length > 0 ? (
+      <div className="MPLP-container">
+        <div className="MPLP-myposts-container">
+          <h1>my closet</h1>
+          <h4>things in my current posession.</h4>
           <ul>
-            {amigosInfo.amigos.map((amigo) => (
-              <li key={amigo._id}>{amigo.username}
-              <button onClick={() => handleDeleteAmigo(amigo._id)}>delete.</button>
-              </li>
-            ))}
+            {posts
+              .filter((post) => post.owner._id === user._id)
+              .map((post) => (
+                <li key={post._id} className="MPLP-myposts-post">
+                  <Link to={`/posts/${post._id}`}>
+                    <img src={post.photo}></img>
+                  </Link>
+                  <h3>{post.title}</h3>
+                </li>
+              ))}
           </ul>
-        ) : (
-          <p>No amigos found.</p>
-        )}
-      </div>
-      <div>
-        <h1>my closet</h1>
-        {closetItems.length > 0 ? (
-          <ul>
-            {closetItems.map((item) => (
-              <li key={item._id}>
-                <Link to={`/posts/${item._id}`}>{item.title}</Link>
-                <button onClick={() => handleRemoveFromCloset(item._id)}>Remove</button>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No items in closet.</p>
-        )}
+        </div>
+        <div className="MPLP-mycloset-container">
+          <h1>our closet.</h1>
+          <h4>things i want to borrow one day.</h4>
+          {closetItems.length > 0 ? (
+            <ul>
+              {closetItems.map((item) => (
+                <li key={item._id} className="MPLP-mycloset-post">
+                  <Link to={`/posts/${item._id}`}>
+                    <img src={item.photo}></img>
+                  </Link>
+                  {item.title}
+                  <button onClick={() => handleRemoveFromCloset(item._id)}>
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No items in closet.</p>
+          )}
+        </div>
+        <div className="MPLP-myamigos-container">
+          <h1>my amigos</h1>
+          <h4>the people i want to share things with.</h4>
+          {amigosInfo.amigos && amigosInfo.amigos.length > 0 ? (
+            <ul>
+              {amigosInfo.amigos.map((amigo) => (
+                <li key={amigo._id} className="MPLP-myamigo">
+                  {amigo.username}
+                  <button onClick={() => handleDeleteAmigo(amigo._id)}>
+                    delete.
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No amigos found.</p>
+          )}
+        </div>
       </div>
     </>
   );
