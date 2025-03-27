62a4346ecb329646b949343c9ac19a34aba67ed3
miriam <maviles221@gmail.com>
2025-03-27 15:49:56 -0500

    home page done styling

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import * as closetService from "../../services/closetService";
 import mctcLogo from "../../assets/mctc_logo.png"; // Import the logo image
@@ -11,33 +11,40 @@ export default function HomePage({ posts, user }) {
       await closetService.addItem(postId); // Call the service function
       setAddedToCloset((prev) => [...prev, postId]); // Update state to track added posts
     } catch (error) {
-      console.error('Error adding post to closet:', error);
+      console.error("Error adding post to closet:", error);
     }
   };
 
   return (
     <>
-    {user ? (
-      <div>
-        <img id="home-logo" src={mctcLogo} alt="mctc logo" />
-        <ul>
-        {posts
-          .map((post) => (
-            <li key={post._id}>
-              <Link to={`/posts/${post._id}`}>{post.title}<img src={post.photo}></img></Link>
-              <button
-                onClick={() => handleAddToCloset(post._id)} // Add post to closet
-                disabled={addedToCloset.includes(post._id)} // Disable button if already added
-              >
-                {addedToCloset.includes(post._id) ? 'Added to Closet' : 'Add to Closet'}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ) : (
-      <h1>Welcome to Mi Closet Ti Closet</h1>
-    )}
+      {user ? (
+        <div className="homepage-container">
+          <img id="home-logo" src={mctcLogo} alt="mctc logo" />
+          <div className="homepage-content-container">
+            <ul>
+              {posts.map((post) => (
+                <li key={post._id} className="homepage-post">
+                  <Link to={`/posts/${post._id}`}>
+                    <img src={post.photo}></img>
+                  </Link>
+                  <h3>{post.title}</h3>
+                  <p>{post.owner.username}</p>
+                  <button
+                    onClick={() => handleAddToCloset(post._id)} // Add post to closet
+                    disabled={addedToCloset.includes(post._id)} // Disable button if already added
+                  >
+                    {addedToCloset.includes(post._id)
+                      ? "Added to Closet"
+                      : "Add to Closet"}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <h1>Welcome to Mi Closet Ti Closet</h1>
+      )}
     </>
-  )
-};
+  );
+}
