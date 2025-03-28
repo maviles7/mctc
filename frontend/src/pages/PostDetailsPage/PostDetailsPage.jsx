fbef8fe6ccb9cc7c0be9426e6d7cb033a78d2097
miriam <maviles221@gmail.com>
2025-03-28 11:47:47 -0500

    post details page CSS done

--- frontend/src/pages/PostDetailsPage/PostDetailsPage.jsx
+++ frontend/src/pages/PostDetailsPage/PostDetailsPage.jsx
@@ -1,85 +1,98 @@
 import { useParams, Link } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import * as postService from '../../services/postService';  
-import * as commentService from '../../services/commentService';
+import * as postService from "../../services/postService";
+import * as commentService from "../../services/commentService";
 
 import CommentForm from "../../components/CommentForm/CommentForm";
 
 const PostDetailsPage = ({ user, handleDeletePost }) => {
-    const { postId } = useParams();
-    console.log('postId:', postId);
+  const { postId } = useParams();
+  console.log("postId:", postId);
 
-    const [post, setPost] = useState(null);
-    console.log('post:', post);
+  const [post, setPost] = useState(null);
+  console.log("post:", post);
 
-    console.log('user:', user);
+  console.log("user:", user);
 
-    useEffect(() => {
-        const fetchPost = async () => {
-            const postData = await postService.show(postId);
-            setPost(postData);
-        };
-        fetchPost();
-    }, [postId]);
-
-    const handleAddComment = async (commentFormData) => {
-        const newComment = await commentService.createComment(postId, commentFormData);
-        setPost({
-            ...post,
-            comments: [...post.comments, newComment],
-        });
+  useEffect(() => {
+    const fetchPost = async () => {
+      const postData = await postService.show(postId);
+      setPost(postData);
     };
+    fetchPost();
+  }, [postId]);
 
-    const handleDeleteComment = async (commentId) => {
-        const deleteComment = await commentService.deleteComment(postId, commentId);
-        setPost({
-            ...post,
-            comments: post.comments.filter((comment) => comment._id !== commentId),
-        });
-    };
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await commentService.createComment(
+      postId,
+      commentFormData
+    );
+    setPost({
+      ...post,
+      comments: [...post.comments, newComment],
+    });
+  };
 
-    if (!post) return <main>oops. post not found.</main>
+  const handleDeleteComment = async (commentId) => {
+    const deleteComment = await commentService.deleteComment(postId, commentId);
+    setPost({
+      ...post,
+      comments: post.comments.filter((comment) => comment._id !== commentId),
+    });
+  };
 
-    return (
-        <>
-            <div>
-                <h1>{post.title} {post.avaibility && '✅'}</h1>
-                <h4>{post.owner.username}</h4>
-                <h2>{post.category}</h2>
-                <h2>{post.size}</h2>
-                <img src={post.photo} />
-                <p>{post.moreInfo}</p>
-            </div>
-            {post.owner._id === user. _id && (
-                <>
+  if (!post) return <main>oops. post not found.</main>;
+
+  return (
+    <>
+      <div className="post-details-page">
+        <div className="PDP-post-container">
+          <div>
+            <h1>
+              {post.title} {post.avaibility && "✅"}
+            </h1>
+            <h4>{post.owner.username}</h4>
+            <h2>{post.category}</h2>
+            <h2>{post.size}</h2>
+            <img src={post.photo} />
+            <p>{post.moreInfo}</p>
+            {post.owner._id === user._id && (
+              <>
                 <Link to={`/posts/${postId}/edit`}>edit.</Link>
-                <button onClick={() => handleDeletePost(postId)}>delete.</button>
-                </>
+                <button onClick={() => handleDeletePost(postId)}>
+                  delete.
+                </button>
+              </>
             )}
-            <div>
-                <h3>comments</h3>
-                <CommentForm handleAddComment={handleAddComment} />
-                {!post.comments.length && <p>no comments.</p>}
-                {post.comments.map((comment) => (
-                    <article key={comment._id}>
-                        <header>
-                            <p>
-                                {comment.owner.username}
-                            </p>
-                        </header>
-                        <h3>{comment.text}</h3>
-                        {comment.owner === user._id && (
-                            <div>
-                                <Link to={`/posts/${postId}/comments/${comment._id}/edit`}>edit comment.</Link>
-                                <button onClick={() => handleDeleteComment(comment._id)}>delete.</button>
-                            </div>
-                        )}
-                        </article>
-                ))}
-            </div>
-        </>
-    )
+          </div>
+        </div>
+        <div className="PDP-comments-container">
+          <h3>comments</h3>
+          <CommentForm handleAddComment={handleAddComment} />
+          {!post.comments.length && <p>no comments.</p>}
+          {post.comments.map((comment) => (
+            <article key={comment._id}>
+              <header>
+                <p>{comment.owner.username}</p>
+              </header>
+              <h3>{comment.text}</h3>
+              {comment.owner === user._id && (
+                <div>
+                  <Link to={`/posts/${postId}/comments/${comment._id}/edit`}>
+                    edit comment.
+                  </Link>
+                  <button onClick={() => handleDeleteComment(comment._id)}>
+                    delete.
+                  </button>
+                </div>
+              )}
+            </article>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PostDetailsPage;
